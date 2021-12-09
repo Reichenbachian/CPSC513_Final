27d10dd34e0bc83f844e943be107898b7a4571a3
danielhodeta <daniel.hodeta@yale.edu>
2021-12-09 06:09:10 -0500

    - Added condition to ignore virus if it's part of the allowlist - Serialization at close for AllowList

--- src/hodalex_antivirus.py
+++ src/hodalex_antivirus.py
@@ -7,23 +7,29 @@ from PyQt5.QtWidgets import QApplication, QFrame, QMainWindow
 from gui import GUI
 from virus_info import VirusSeverity
 from datetime import datetime
-from util.quarantinedfile import QuarantinedFile, QFileList
+from util.quarantinedfile import QuarantinedFile
+from util.datalists import AllowList, QFileList
 
 
 def run_folder_scan(folder):
 	counter_measures = CounterMeasures(config.QUARANTINE_FOLDER)
 	for scanner in SCANNERS:
 		for virus, virus_info in scanner.scan_folder(folder):
-			if (virus_info == VirusSeverity.QUARANTINE):
-				perm = oct(os.stat(virus).st_mode)
-				qfile = QuarantinedFile(os.path.basename(virus), virus, perm, datetime.now())
-				QFileList.insert(0, qfile)
-			counter_measures.address_virus(virus, virus_info)
+			if (not AllowList.find(virus)):
+				if (virus_info == VirusSeverity.QUARANTINE):
+					perm = oct(os.stat(virus).st_mode)
+					qfile = QuarantinedFile(os.path.basename(virus), virus, perm, datetime.now())
+					QFileList.insert(0, qfile)
+				counter_measures.address_virus(virus, virus_info)
 
 def print_help():
     ctx = click.get_current_context()
     click.echo(ctx.get_help())
     ctx.exit()
+
+def serialize_lists():
+	QFileList.save()
+	AllowList.save()
 
 @click.command()
 @click.option('-s', '--scan', 'scan_folder', type=str, help='Scan a specific folder.')
@@ -33,7 +39,7 @@ def main(scan_folder, gui):
 		run_folder_scan(scan_folder)
 	elif gui:
 		app = QApplication([])
-		app.aboutToQuit.connect(lambda: QFileList.save())
+		app.aboutToQuit.connect(serialize_lists)
 		window = QMainWindow()
 		g = GUI(window)
 		window.show()
