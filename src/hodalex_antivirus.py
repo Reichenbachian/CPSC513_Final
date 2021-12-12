65608d42db71d9fcac18089379e39513157c9aed
danielhodeta <daniel.hodeta@yale.edu>
2021-12-11 22:06:25 -0500

    Added a daemon bg service that runs alongside the GUI

--- src/hodalex_antivirus.py
+++ src/hodalex_antivirus.py
@@ -1,6 +1,8 @@
 import os
+import threading
 import click
 import config
+import time
 from scanner import SCANNERS
 from counter_measures import CounterMeasures
 import numpy as np
@@ -33,14 +35,33 @@ def serialize_lists():
 	AllowList.save()
 	ScanScheduleList.save()
 
+def background_service():
+	scan_folder = config.TEST_VIRUS_DIRECTORY
+	ScanScheduleList.load()
+	while(True):
+		if (ScanScheduleList.len() > 0):
+			next = ScanScheduleList.get(0)
+			next.wait()
+			ScanScheduleList.remove(next)
+			if (next.next_schedule()):
+				ScanScheduleList.bisect_insort(next)
+			bg_scan = threading.Thread(target=run_folder_scan, args=[scan_folder])
+			bg_scan.start()
+			bg_scan.join()
+		time.sleep(2)
+
 @click.command()
 @click.option('-s', '--scan', 'scan_folder', type=str, help='Scan a specific folder.')
 @click.option('-g', '--gui/--no-gui', 'gui', help='Scan a specific folder.')
-def main(scan_folder, gui):
+def main(scan_folder, gui, bg = None):
+
 	if scan_folder is not None:
-		run_folder_scan(scan_folder)
+		run_folder_scan()
 		serialize_lists()
 	elif gui:
+		bg_scan = threading.Thread(target=background_service)
+		bg_scan.setDaemon(True)
+		bg_scan.start()
 		app = QApplication([])
 		app.aboutToQuit.connect(serialize_lists)
 		window = QMainWindow()
@@ -49,6 +70,7 @@ def main(scan_folder, gui):
 		app.exec()
 	else:
 		print_help()
+	
 
 if __name__ == "__main__":
 	main()
